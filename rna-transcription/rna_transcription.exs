bef119ab6937425ec7af96e8717116dd04868566
Rodrigo Martins <rrmartinsjg@gmail.com>
2019-02-17 23:18:03 -0300

    code transcription

---
+++ rna-transcription/rna_transcription.exs
@@ -0,0 +1,20 @@
+defmodule RNATranscription do
+  @dna_to_rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, &(@dna_to_rna[&1]))
+  end
+end
